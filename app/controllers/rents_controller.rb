13b6755a62babfb15eb44d1b5af5c342a99558f6
DreamsLord <adamludian@gmail.com>
2017-10-25 20:17:08 +0200

    Set rent book

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -1,25 +1,23 @@
 class RentsController < ApplicationController
-  before_action :set_rent, only: %i[show edit update destroy]
+  before_action :set_rent, only: %i[show edit update destroy return_book]
   def index
     @rents = current_user.rents.all.page params[:page]
   end
 
   def show; end
 
-  def edit; end
-
-  def update
-    if @rent.update(rent_params)
-      flash.now[:notice] = 'rent modificated'
-      redirect_to @rent
-    else
-      flash.now[:error] = 'Failed to modificated rent in database'
-      render :edit
-    end
+  def edit;
+    update
   end
 
-  def destroy
-    @rent.destroy
+  def update
+    if @rent.update(return_params)
+      flash.now[:notice] = 'book returned'
+      redirect_to @rent
+    else
+      flash.now[:error] = 'Failed to return book'
+      render :edit
+    end
   end
 
   def new
@@ -30,9 +28,9 @@ class RentsController < ApplicationController
     @rent = current_user.rents.new(rent_params)
     if @rent.save
       redirect_to @rent
-      flash.now[:notice] = 'rent has been created'
+      flash.now[:notice] = 'book has been rented'
     else
-      flash.now[:error] = 'Failed to save rent'
+      flash.now[:error] = 'Failed to rent the book'
       redirect_to new_rent_url(@rent)
     end
   end
@@ -44,6 +42,10 @@ class RentsController < ApplicationController
   end
 
   def rent_params
+    params.require(:rent).permit(:user_id, :book_id)
+  end
+
+  def return_params
     params.require(:rent).permit(:user_id, :book_id, :return?)
   end
 end
--- app/models/rent.rb
+++ app/models/rent.rb
@@ -17,4 +17,5 @@ class Rent < ApplicationRecord
   belongs_to :user
   belongs_to :book
   validates :user_id, :book_id, :return_date, :return?, presence: true
+
 end
