f4c6609755f11bba8c93a8afc114991d7a42b2f8
DreamsLord <adamludian@gmail.com>
2017-10-25 12:49:45 +0200

    Add model book tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -14,5 +14,23 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "database columns" do
+    it {should have_db_column(:id) }
+    it {should have_db_column(:name) }
+    it {should have_db_column(:author) }
+    it {should have_db_column(:category) }
+    it {should have_db_column(:description) }
+    it {should have_db_column(:created_at) }
+    it {should have_db_column(:updated_at) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:author) }
+    it { should validate_length_of(:name).is_at_least(1).is_at_most(255) }
+    it { should validate_length_of(:author).is_at_least(1).is_at_most(255) }
+    it { should validate_length_of(:category).is_at_least(1).is_at_most(255) }
+    it { should validate_length_of(:description).is_at_least(25).is_at_most(6000) }
+
+  end
 end
